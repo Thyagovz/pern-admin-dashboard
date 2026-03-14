48b5bb931e2b090e8e701abd5833a38ecb4e198c
Thyago Euclides <flooddk@gmail.com>
2026-03-14 10:27:07 -0400

    implement suggestions from coderabit

--- classroom-backend/src/index.ts
+++ classroom-backend/src/index.ts
@@ -1,17 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
-import { demoUsers } from './db/schema/index.js';
 import subjectsRouter from "./routes/subjects.js";
-import {db} from "./db/index.js";
 import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+if (!process.env.FRONTEND_URL) {
+    throw new Error("FRONTEND_URL is not set in .env file");
+}
+
 app.use(cors({
-  origin: process.env.FRONTEND_URl,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
 }))
 
 app.use(express.json());
@@ -19,22 +21,9 @@ app.use(express.json());
 app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Classroom API!');
+    res.send('Welcome to the Classroom API!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-async function main() {
-  try {
-    console.log('Checking database connection...');
-    const result = await db.select().from(demoUsers).limit(1);
-    console.log('✅ Database connection successful.');
-  } catch (error) {
-    console.error('❌ Database connection error:', error);
-  } finally {
-  }
-}
-
-main();
